3137309bcb01ceda648ce668a27fea24820d21fa
ds1242 <david.shaw1242@gmail.com>
2021-07-28 15:32:29 -0600

    add reaction functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ReactionList from '../components/ReactionList';
-
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -34,6 +35,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
